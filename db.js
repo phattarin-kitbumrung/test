25f8eaea5da1ef967203ad92269fb42463037bc3
phattarin kitbumrung <pm.phobia@gmail.com>
2017-07-25 13:24:31 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 var mongojs = require('mongojs');
-//var databaseUrl = 'sobtid_query:edxXEP89@db4757.sobtid.me/sobtiddb';
 var databaseUrl = '54.169.110.164:27017/minizymint';
 var collections = ['todolist'];
 var connect = mongojs(databaseUrl, collections);
